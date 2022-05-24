5cab7fa7fb2ccc39875f609b4a6c77b606187d65
Pramish Shrestha <pramishstha22@gmail.com>
2022-05-24 23:08:45 +0545

    Initial Project Setup

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Reddit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default Home
